c1771bde42d0721e7a1bbfa655a39ed555fdcc3b
Elliot Schlegelmilch <elliot@pdx.edu>
2012-01-04 18:15:21 -0800

    autogen of settings.php based on site. add status flags.

--- deploy/models.py
+++ deploy/models.py
@@ -23,6 +23,12 @@ class Platform(models.Model):
     def __unicode__(self):
         return self.name
 
+class Status(models.Model):
+    status = models.CharField(max_length=48, primary_key=True)
+    def __str__(self):
+        return self.status
+    def __unicode__(self):
+        return self.status
 
 class Site(models.Model):
 
@@ -32,9 +38,8 @@ class Site(models.Model):
     database         = models.CharField(max_length=32, null=False, blank=True)
     contact_email    = models.CharField(max_length=64, help_text='this populates the site_admin field of the site')
     staff_email      = models.CharField(max_length=64)
+    status           = models.ManyToManyField(Status)
 
-    pre_production   = models.BooleanField(default=True, help_text='Is this a site that has never been deployed to production.')
-    maintenance_mode = models.BooleanField()
 
     def save(self, *args, **kwargs):
         if self.database == '':
@@ -46,6 +51,19 @@ class Site(models.Model):
 
     def __unicode__(self):
         return u"http://%s/%s" %( self.platform.host, self.short_name )
+
+    # some helpers for the multiflag.
+    def show_status(self):
+        return ','.join([str(i) for i in self.status.all()])
+
+    @property
+    def installed(self):
+        return (self.status.filter(status = 'not installed').count() == 0)
+
+    @property
+    def local_config(self):
+        return None
+        
 
     def site_dir(self):
         return os.path.join(self.platform.path, 'sites', self.platform.host + '.' +  self.short_name)
@@ -64,3 +82,33 @@ class Site(models.Model):
 
     def site_symlink(self):
         return os.path.join(self.platform.path, self.short_name)
+
+    def settings_php(self, f=None):
+        inc = os.path.join(self.platform.path, self.platform.host + '.php')
+        settings = """<?php
+$_db  = '%(database)s';
+$_dir = '%(sitedir)s'; 
+$install = %(install)s;
+
+$_inc = '%(inc)s';
+require_once($_inc);
+
+#do not modify this line: local changes below this point
+
+%(local_config)s
+"""
+        site_vars = {
+            'database': self.database,
+            'sitedir': self.platform.host + '.' +  self.short_name,
+            'install': ('NULL' if self.installed else 'TRUE'),
+            'inc': inc,
+            'local_config': self.local_config }
+            
+        print settings
+            
+        print site_vars
+
+
+        return settings % site_vars
+
+
