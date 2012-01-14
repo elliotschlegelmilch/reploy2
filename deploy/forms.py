a6b418a424b6195ce4ee57f8a855e5db6bf1ddf0
Elliot Schlegelmilch <elliot@pdx.edu>
2012-01-14 15:16:57 -0800

    pull together migrate form.

--- deploy/forms.py
+++ deploy/forms.py
@@ -1,8 +1,10 @@
 from django.forms import ModelForm
 from django import forms
-
 from deploy.models import Platform
 
 class Migrate(forms.Form):
-    new_platform = forms.ModelMultipleChoiceField( queryset=Platform.objects.all() )
+    new_platform = forms.ModelChoiceField( queryset=Platform.objects.all(),
+                                           #widget=Select(
+                                           #attrs={'size':'1'}, choices=Platform.objects.all())
+                                                  )
     
--- deploy/views.py
+++ deploy/views.py
@@ -1,12 +1,33 @@
-from django.http import HttpResponse
+from deploy.actions import migrate
 from deploy.forms import Migrate
+from deploy.models import Platform, Site
 
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
 
-def site_migrate(request, d):
-    
-    f = Migrate()
-    x = " %s " %( f,) 
-    return HttpResponse( x)
+#@csrf_protect
+def site_migrate(request):
+
+    form = Migrate(request.POST if request.POST else None)
+
+    if form.is_valid():
+        #what sites:
+        l = request.GET['ids'].split(',')
+        site_ids = [int(i) for i in l]
+        sites = Site.objects.filter(pk__in=site_ids)
+        platform = Platform.objects.get(pk=request.POST['new_platform'])
+        for site in sites:
+            migrate(site, platform)
+        
+    data = {
+        'user': request.user,
+        'form': form,
+        }
+
+    resp = render_to_response('foo.html', data)
+    return resp
+
 
 
 def home(request):
