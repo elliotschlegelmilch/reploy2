8e3da833e2ca5d8eb7c6fe36b1e7dbc0961e2dc9
Elliot Schlegelmilch <elliot@pdx.edu>
2012-02-07 16:53:19 -0800

    add the view site_drush()

--- deploy/views.py
+++ deploy/views.py
@@ -1,5 +1,5 @@
-from deploy.actions import migrate
-from deploy.forms import Migrate
+from deploy.actions import migrate, drush
+from deploy.forms import Migrate, Drush
 from deploy.models import Platform, Site, Event
 from django.conf import settings
 from django.contrib import messages
@@ -41,6 +41,37 @@ def site_migrate(request):
     return render_to_response('migrate.html', data)
 
 
+def site_drush(request):
+
+    form = Drush(request.POST if request.POST else None)
+
+    if form.is_valid():
+        #what sites:
+        l = request.GET['ids'].split(',')
+        site_ids = [int(i) for i in l]
+        sites = Site.objects.filter(pk__in=site_ids)
+
+        
+        cmd = request.POST['drush_command']
+        for site in sites:
+            ctask = drush.delay(site, cmd)
+            event = Event( task_id=ctask.task_id, site=site, user=request.user, event='drush')
+            event.save()
+            messages.add_message(request, messages.INFO, "The drush command on the site %s has been queued: %s" % ( site, ctask.task_id) )
+
+        # this needs to redirect or something.
+        return redirect(
+            urlresolvers.reverse('admin:deploy_site_changelist')
+            )
+         
+    data = {
+        'user': request.user,
+        'form': form,
+        }
+
+    return render_to_response('drush.html', data)
+
+
 def platform_status(request, platform=None):
     p = get_object_or_404( Platform, pk=platform)
     _heading = ['url', 'short_name', 'long_name', 'database', 'contact_email']
@@ -53,7 +84,7 @@ def platform_status(request, platform=None):
     writer.writerow( _heading )
 
     for s in Site.objects.filter(platform = p):
-        writer.writerow([ s.__getattribute__(column) for column in _heading ])
+        writer.writerow([ s.__getattribute__(column).encode('ascii','replace') for column in _heading ])
 
     return response
 
