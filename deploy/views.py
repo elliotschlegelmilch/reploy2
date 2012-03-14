112c94d6e62fbd6251a033dfb82169dbb7dd7b5b
Elliot Schlegelmilch <elliot@pdx.edu>
2012-03-14 16:33:00 -0700

    ugly first attempt for the site-manage screen to actually do something.

--- deploy/views.py
+++ deploy/views.py
@@ -1,4 +1,5 @@
-from deploy.actions import migrate, rename, drush, update_events, update_statistic, get_site_status
+from deploy.actions import migrate, rename, drush, update_events, update_statistic, get_site_status, enable, disable, cacheclear, verify, varnishclear
+
 from deploy.forms import Migrate, Clone, Drush
 from deploy.models import Platform, Site, Event, Statistic
 from django.conf import settings
@@ -12,25 +13,54 @@ import datetime
 import json
 
 def site_manage(request, sid):
+    print request.POST
     site = get_object_or_404( Site, pk=sid)
     events = Event.objects.filter( site= site ).order_by('date')
     callbacks = Event.objects.filter( site= site, event='status' ).order_by('date')
-    
+   
     task = get_site_status.delay(site)
-    status_event = Event( task_id=task.task_id,
-                          site=site,
-                          user=request.user,
-                          event="statistic")
-    status_event.save()
+
+    #status_event = Event( task_id=task.task_id, site=site, user=request.user, event="statistic")
+    #status_event.save()
+
+    op =  request.POST.get('submit', None)
+
+    if not op == None:
+        if op == 'enable':
+            enable.delay(site)
+        elif op == 'disable':
+            disable.delay(site)
+        elif op == 'cache':
+            cacheclear.delay(site)
+        elif op == 'varnish':
+            varnishclear.delay(site)
+        elif op == 'verify':
+            verify.delay(site)
+        elif op == 'migrate':
+            migrate.delay(site, request.POST.get('new_platform'))
+        elif op == 'clone':
+            pass
+        elif op == 'rename':
+            pass
+    
+
+
+    forms = {'clone': Clone(),
+             'migrate': Migrate(),
+             'drush': Drush(),
+             }
+
 
     update_events()
     update_statistic()
     
-    data = {'events': events,
-            'user'  : request.user,
-            'site'  : site,
+    data = {'events'    : events,
+            'user'      : request.user,
+            'site'      : site,
             'callbacks' : callbacks,
+            'forms'     : forms
             }
+    
     return render_to_response('site-manage.html', data)
     
 
